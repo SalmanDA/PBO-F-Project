87757a9f1ed15fc8f3cf3af6b9a970368c5b67d0
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2020-12-21 11:36:14 +0700

    Add Code Program
    Kalau salah revisi rek

---
+++ Collision Detection with Mouse/Collision Detection with Mouse/src/id/ac/its/CollisionDetectionwithMouse/SpaceShip.java
@@ -0,0 +1,68 @@
+package id.ac.its.CollisionDetectionwithMouse;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.MouseEvent;
+
+public class SpaceShip extends Sprite {
+
+	private int dx;
+    private int dy;
+    private int MAX_X;
+    private int MAX_Y;
+    private List<Missile> missiles;
+
+    public SpaceShip(int x, int y) {
+        super(x, y);
+
+        initCraft();
+    }
+
+    private void initCraft() {
+        
+        missiles = new ArrayList<>();
+        loadImage("gambar/pesawat.png");
+        getImageDimensions();
+        
+        this.MAX_X = 400 - getWidth();
+        this.MAX_Y = 300 - getHeight();
+    }
+
+    public void move() {
+
+        x = dx;
+        y = dy;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
+    }
+
+    public List<Missile> getMissiles() {
+        return missiles;
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    	fire();
+    }
+    
+    public void mouseDragged(MouseEvent e) {
+    	fire();
+    	mouseMoved(e);
+    }
+    
+    public void mouseMoved(MouseEvent e) {
+    	if(e.getX() < MAX_X) dx = e.getX();
+    	if(e.getY() < MAX_Y) dy = e.getY();
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+	
+}
